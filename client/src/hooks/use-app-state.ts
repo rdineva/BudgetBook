d9b23c0675b9b88ffcf1f20c06e0943da5fe0a9b
Krasimira Konstantinova <krasi@hlebarov.com>
2021-01-09 19:29:02 +0200

    Client initial commit

---
+++ client/src/hooks/use-app-state.ts
@@ -0,0 +1,6 @@
+import { useSelector } from 'react-redux';
+import { AppState } from '../store';
+
+export default function useAppState<TSelected>(selector: (state: AppState) => TSelected) {
+return useSelector<AppState, TSelected>(selector);
+}
---
+++ client/src/store/index.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+});
+
+export type AppState = ReturnType<typeof rootReducer>;
+
+export default rootReducer;
